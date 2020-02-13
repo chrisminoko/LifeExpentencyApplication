56d892d80742fea7d53e3b2679c76e75c393836e
M Edoka (21750647) <21750647@dut4life.ac.za>
2020-02-13 09:41:08 +0200

    Add project files.

---
+++ LifeExpentencyApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LifeExpentencyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
